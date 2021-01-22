3aa9ac20ad07c04a058e0e03c065f1c958043048
VBaykin <vbaykin@mail.ru>
2021-01-22 19:40:13 +0300

    для построения установочного архива

--- build.bat
+++ build.bat
@@ -1,1 +1,11 @@
-c:\Harbour\bin\hbmk2 chip_mo.hbp
+c:\Harbour\bin\hbmk2 chip_mo_bay.hbp
+copy chip_mo.exe d:\_mo\_arc
+copy *.shb  d:\_mo\_arc
+copy *.frm  d:\_mo\_arc
+copy *.fr3  d:\_mo\_arc
+copy *.xls  d:\_mo\_arc
+copy chip_mo.exe d:\_mo\chip\exe
+
+if exist d:\_mo\_build\chip_mo.rar del d:\_mo\_build\chip_mo.rar
+copy d:\_mo\_arc\readme.rtf d:\_mo\_build\readme.rtf
+C:\"Program Files"\WinRAR\Rar.exe a -ep d:\_mo\_build\chip_mo @d:\_mo\2.x\chip_mo_bay.lst
