cb01bf38657dc5a5b3653294ade2b4b9778bdbeb
chipcom <vbaykin@mail.ru>
2026-05-20 08:48:17 +0300

    очистка plugins_prg

--- plugins_prg/build_hrb.bat
+++ plugins_prg/build_hrb.bat
@@ -6,10 +6,9 @@ harbour monitoring_vid_pom.prg -n -gh -q -Ic:\harbour\include;..\..\include
 harbour b_25_perinat_2.prg -n -gh -q -Ic:\harbour\include;..\..\include
 harbour pr_inog_inostr_new.prg -n -gh -q -Ic:\harbour\include;..\..\include
 harbour correct_vidpom.prg -n -gh -q -Ic:\harbour\include;..\..\include
-harbour writexlsx_inf_pn.prg -n -gh -q -Ic:\harbour\include;..\..\include
 
 copy *.hrb d:\_mo\chip\exe\plugins
 copy chip_plugin.ini d:\_mo\chip\exe
 
-rem copy *.hrb d:\_mo\_arc\plugins
-rem copy chip_plugin.ini d:\_mo\_arc
+copy *.hrb d:\_mo\_arc\plugins
+copy chip_plugin.ini d:\_mo\_arc
